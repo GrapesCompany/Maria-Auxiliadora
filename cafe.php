bdca52e5c4668be0927215d5931beab82d780679
JChoto <jhonatandcm@hotmail.com>
2018-08-09 19:52:02 -0500

    falta vendor

---
+++ cafe.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/pro.css" rel="stylesheet">
+
+
+    <!-- Custom styles -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+
+<script language="javascript" type="text/javascript">
+function enviar(pagina){
+document.nform.action = pagina;
+document.nform.submit();}
+</script>
+
+</head>
+
+<body>
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <div>
+            <p class="dise">
+                    Dispensario María Auxiliadora
+                </p>
+            </div>
+            <div class="fadeIn first">
+                <img src="https://i.pinimg.com/originals/9d/9b/ab/9d9bab48cbb3a5de959ab844b3e9712d.gif" id="icon" alt="User Icon" />
+            </div>
+
+            <?php
+
+            if(!isset($_SESSION['user'])){?>            
+
+            <form name="nform" action="login.php" method="post">
+                <input type="text" id="login" class="fadeIn second" name="username" placeholder="Usuario">
+                <input type="text" id="password" class="fadeIn third" name="password" placeholder="Contraseña">
+                <input type="button" class="fadeIn fourth" value="Entrar" onClick="enviar('/pan.php')">
+            </form>
+
+            <?php } ?>
+             
+            <div id="formFooter">
+                <a class="underlineHover" href="#">¿Olvidaste la contraseña?</a>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
