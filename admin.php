838dd5766f9c4f3ef45760470eaf9e3ce627b299
JChoto <jhonatandcm@hotmail.com>
2018-08-23 18:10:01 -0500

    changes

--- admin.php
+++ admin.php
@@ -95,15 +95,23 @@
       <!-- Sidebar -->
       <ul class="sidebar colorsec navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Inicio</span>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Nuevo</span>
           </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            
+            <a class="dropdown-item" href="#">Médico</a>
+            <a class="dropdown-item" href="#">Auxiliar</a>             
+          </div>
         </li>
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-fw fa-folder"></i>
             <span>Páginas</span>
+
           </a>
           <div class="dropdown-menu" aria-labelledby="pagesDropdown">
             <h6 class="dropdown-header">Login Screens:</h6>
