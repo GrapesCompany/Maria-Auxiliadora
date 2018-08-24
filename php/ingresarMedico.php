194dfe8c4850ae6b3e4ea1dad06618aa2bc97a87
stteffano <stteffano_13@hotmail.com>
2018-08-23 22:08:48 -0500

    sAS

--- php/ingresarMedico.php
+++ php/ingresarMedico.php
@@ -9,8 +9,8 @@ $
     echo "hola tefo", $cedula;
         $conexion=conectar();
          
-         mysqli_query( $conexion, "insert into medico(CEDULA_MED,NOMBRE_MED,TELEFONO_MED,ESPECIALIDAD,ESTADO_MED,CONTRASENA_MED) values
-           ('$cedul','1','$this->email',' $this->direccion','  $this->telefono',' $this->clave',' $this->imagen')" ) or die("Problemas en el select".mysqli_error($conexion));
+         mysqli_query( $conexion, "insert into medico(CEDULA_MED,NOMBRE_MED,APELLIDO_MED,TELEFONO_MED,ESPECIALIDAD,ESTADO_MED,CONTRASENA_MED) values
+           ('$cedula','$nombre','$apellido',' $this->direccion','  $this->telefono',' $this->clave',' $this->imagen')" ) or die("Problemas en el select".mysqli_error($conexion));
  
              return $u;
          
