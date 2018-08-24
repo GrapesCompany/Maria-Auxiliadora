903f944085b3b4bda50c52ad6cdd58b80c715431
JChoto <jhonatandcm@hotmail.com>
2018-08-24 15:26:01 -0500

    Reparacion

--- js/funciones.js
+++ js/funciones.js
@@ -49,22 +49,7 @@
 }
 
 
-function showPaciente(str) {
 
-    document.getElementById("div_modificaraux").innerHTML = ""; 
-    document.getElementById("div_modificarcita").innerHTML = "";  
-
-   var xmlhttp = new XMLHttpRequest();
-   xmlhttp.onreadystatechange = function () {
-       if (this.readyState == 4 && this.status == 200) {
-           document.getElementById("div_nuevopaciente").innerHTML = this.responseText;
-           
-       }
-   }
-
-   xmlhttp.open("GET", "nuevo_paciente.php?q=" + str, true);
-   xmlhttp.send();
-}
 
 function showModificarAuxiliar(str) {
 
@@ -79,6 +64,23 @@ function showModificarAuxiliar(str) {
    }
 
    xmlhttp.open("GET", "modificar_auxiliar.php?q=" + str, true);
+   xmlhttp.send();
+}
+
+function showPaciente(str) {
+
+    document.getElementById("div_modificaraux").innerHTML = ""; 
+    document.getElementById("div_modificarcita").innerHTML = "";  
+
+   var xmlhttp = new XMLHttpRequest();
+   xmlhttp.onreadystatechange = function () {
+       if (this.readyState == 4 && this.status == 200) {
+           document.getElementById("div_nuevopaciente").innerHTML = this.responseText;
+           
+       }
+   }
+
+   xmlhttp.open("GET", "nuevo_paciente.php?q=" + str, true);
    xmlhttp.send();
 }
 
