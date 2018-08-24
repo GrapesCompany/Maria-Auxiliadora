d2f8503846ab66b4a27478ebe546a8f39a12dc39
stteffano <stteffano_13@hotmail.com>
2018-08-23 20:26:29 -0500

    ConexionFunciones

--- conexion.php
+++ conexion.php
@@ -3,5 +3,38 @@
     $username = "root";
     $password = "";
     $db = "db_consultorio";
-    $con = mysqli_connect($server, $username, $password,$db) or die("No se ha podido establecer la conexión");
+    
+
+
+
+
+ 
+    
+
+    function conectar()
+    
+    {
+        $conexion = mysqli_connect($server, $username, $password,$db) or die("No se ha podido establecer la conexión");
+          
+        mysqli_select_db( $conexion, $db)or die("Problemas en la selección de la base de datos");
+       
+        return $conexion;
+          
+    }
+    
+    
+    
+                
+     
+     function cerrar($conexion)
+     {
+        mysqli_close($conexion); 
+      
+     }
+      
+    
+
+    
+    
+
 ?>
